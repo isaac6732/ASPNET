ddedffa2de7972c3ad3540317c7348c9af3a5e8c
Isaac Oquendo <ioalvarez378@gmail.com>
2024-03-03 17:42:13 -0500

    ASP

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Testing.Models;
+using System;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
