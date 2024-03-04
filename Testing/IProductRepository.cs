fc21734b9c57fc20f61aca989991551c62dee624
Isaac Oquendo <ioalvarez378@gmail.com>
2024-03-03 21:32:47 -0500

    apsN

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -7,5 +7,9 @@ namespace Testing
     public interface IProductRepository
     {
         public IEnumerable<Product> GetAllProducts();
+
+        public Product GetProduct(int id);
+
+        public void UpdateProduct(Product product);
     }
 }
